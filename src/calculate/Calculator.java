d7300436a17a81fde34133fdd60d1144bf28d6ca
akishere <akmalik.malik876@gmail.com>
2019-07-29 13:12:40 +0530

    Initial Commit

---
+++ src/calculate/Calculator.java
@@ -0,0 +1,31 @@
+package calculate;
+
+public class Calculator {
+	
+		public int x;
+		public int y;
+		
+		public int add(int x,int y) {
+			return (x+y);
+		}
+		public int subtract(int x,int y) {
+			return(x-y);
+		}
+		public int multiply(int x,int y) {
+			return(x*y);
+			
+		}
+		public int divide(int x,int y) {
+			return(x/y);
+		}
+
+
+public static void main(String[] args) {
+	Calculator cal = new Calculator();
+	System.out.println(cal.add(3, 4));
+	System.out.println(cal.subtract(4, 3));
+	System.out.println(cal.multiply(3, 4));
+	System.out.println(cal.divide(4, 4));
+}
+}
+
